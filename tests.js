45af5e241e9d57089c972281993bb40c3c62835a
Nooma <tom_bradley123@hotmail.co.uk>
2018-11-30 09:45:05 +0000

    phantomJS for unit testing via jenkins

--- tests.js
+++ tests.js
@@ -1,3 +1,11 @@
 QUnit.test("Simple test to ensure page title is correct", function(assert){
     assert.equal(document.getElementsByTagName("title")[0].innerHTML, "SOFT352 Quiz","Ensuring SOFT352 is the title.");
 });
+
+QUnit.jUnitDone(function(data) {
+	var console = window.console;
+	if (console) {
+		document.output = data.xml;
+		console.log(data.xml);
+	}
+});
